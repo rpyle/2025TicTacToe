b7fb4fd2d27b8b9f5bf431c542e73ad82317916b
adolanr <366933@guhsd.net>
2025-03-18 11:17:53 -0700

    Added dolan.py and changed strategy in dolan.py

---
+++ dolan.py
@@ -0,0 +1,16 @@
+import random
+team_name = 'dolan'
+strategy_name = 'Center then next open'
+strategy_description = 'Play center if available, then next available'
+    
+def move(player, board, score):
+  r = 1
+  c = 1
+
+  while board[r][c] != ' ':
+    c = c + 1
+    if c > 2:
+      c = 0
+      r = r + 1
+  
+  return r, c
