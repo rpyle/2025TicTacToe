1d3c917cbd5af35130ec03d4476bca01c45e7019
Lorelei Abboud <365145@guhsd.net>
2025-03-18 11:50:39 -0700

    updated strategy

--- abboud.py
+++ abboud.py
@@ -1,3 +1,4 @@
+import random
 team_name = 'Abboud'
 strategy_name = 'Middle-Slash'
 strategy_description = 'Play the next open spot.'
@@ -11,12 +12,53 @@ def print_board(board):
   print(print_board)
 
 def move(player, board, score):
-  r = 2
-  c = 2
-  while board[r][c] != ' ':
-    c = c - 1
-    if c < 0:
-      c = 2
-      r = r - 1
+  r = 1
+  c = 1
+  if board[r][c] == ' ':
+    return r, c
   
+  r = 0 
+  c = 0
+  if board[r][c] == ' ' and board [1][1] == player:
+    return r, c
+  if board [r][c] == player and board [1][1] == player:
+    r = 2
+    c = 2
+    if board[r][c] == ' ':
+        return r, c
+  
+  r = 0 
+  c = 2
+  if board[r][c] == ' ' and board [1][1] == player:
+    return r, c
+  if board [r][c] == player and board [1][1] == player:
+    r = 2
+    c = 0
+    if board[r][c] == ' ':
+        return r, c
+
+  r = 0 
+  c = 1
+  if board[r][c] == ' ' and board [1][1] == player:
+    return r, c
+  if board [r][c] == player and board [1][1] == player:
+    r = 2
+    c = 1
+    if board[r][c] == ' ':
+        return r, c
+
+  r = 1 
+  c = 0
+  if board[r][c] == ' ' and board [1][1] == player:
+    return r, c
+  if board [r][c] == player and board [1][1] == player:
+    r = 1
+    c = 2
+    if board[r][c] == ' ':
+        return r, c    
+
+  while board[r][c] != ' ':
+    r = random.randint(0,2)
+    c = random.randint(0,2)
+
   return r, c
