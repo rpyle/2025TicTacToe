0ff84b9febd339149133214d6ef91993c9f2388a
Kyle Coon <367346@guhsd.net>
2025-03-18 11:27:50 -0700

    added coon.py

---
+++ coon.py
@@ -0,0 +1,22 @@
+team_name = 'coon'
+strategy_name = 'Last Open'
+strategy_description = 'Play the last open spot.'
+
+def print_board(board):
+  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
+  print('-+-+-')
+  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
+  print('-+-+-')
+  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
+  print(print_board)
+
+def move(player, board, score):
+  r = 2
+  c = 2
+  while board[r][c] != ' ':
+    c = c - 1
+    if c < 0:
+      c = 2
+      r = r - 1
+  
+  return r, c
