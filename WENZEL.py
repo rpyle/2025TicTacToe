a5d24d2c40465b5e7679aae7b30aaa8c5e3d4f25
WENZEL <374442.guhsd.net>
2025-03-18 16:14:23 -0700

    Fixed the WENZEL strat logic

--- WENZEL.py
+++ WENZEL.py
@@ -14,23 +14,23 @@ def print_board(board):
 def move (player, board, score):
   r = 0
   c = 0
-  if board[1][1] = ' ':
+  if board[1][1] == ' ':
     r = 1 
     c = 1
-  elif board[0][0] = ' ':
+  elif board[0][0] == ' ':
     r = 0
     c = 0
-  elif board[0][2] = ' ':
+  elif board[0][2] == ' ':
     r = 0
     c = 2
-  elif board[2][0] = ' ':
+  elif board[2][0] == ' ':
     r = 2
     c = 0
-  elif board[2][2] = ' ':
+  elif board[2][2] == ' ':
     r = 2
     c = 2
   else:
-    while board[r][c] = ' ':
+    while board[r][c] != ' ':
       r = random.randint(0,2)
       c = random.randint(0,2)
   return (r,c)
