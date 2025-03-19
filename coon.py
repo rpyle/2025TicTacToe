509b24b1d0d61ed73a46dcd3f7e3939a1d8e7310
Kyle Coon <367346@guhsd.net>
2025-03-19 11:52:24 -0700

    updated coon.py

--- coon.py
+++ coon.py
@@ -1,7 +1,6 @@
 team_name = 'coon'
-strategy_name = 'Last Open'
-strategy_description = 'Play the last open spot.'
-import random
+strategy_name = 'CMCS'
+strategy_description = 'It playes the first corner, then the middle, followed by the other corners, and then the sides.'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -12,16 +11,12 @@ def print_board(board):
   print(print_board)
 
 def move(player, board, score):
-  r = 2
-  c = 2
-  l = 1
-  cornerlist = [0, 2]
+  r = 0
+  c = 0
+  rlist = [2, 1, 0, 1, 2, 2, 0, 1]
+  clist = [1, 2, 1, 0, 2, 0, 2, 1]
 
   while board[r][c] != ' ':
-    c = random(cornerlist)
-    r = random(cornerlist)
-    l = l + 1
-    if l > 10:
-      c = random 
-  
+    r = rlist.pop()
+    c = clist.pop()
   return r, c
