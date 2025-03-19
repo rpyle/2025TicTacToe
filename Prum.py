a537ddd61b0dbbc53995ff075f9669a307b5e0c4
Jayden Prum <364877@guhsd.net>
2025-03-19 12:03:32 -0700

    hi

--- Prum.py
+++ Prum.py
@@ -1,14 +1,15 @@
-import random
 team_name = 'Prum'
-strategy_name = 'Random'
-strategy_description = 'Play a random open spot.'
-    
-def move(player, board, score):
-  r = random.randint(0,2)
-  c = random.randint(0,2)
+strategy_name = 'strafe'
+strategy_description = 'check each spot to see which one will create a straight line'
 
-  while board[r][c] != ' ':
-    r = random.randint(0,2)
-    c = random.randint(0,2)
-  
-  return r, c
+def move(player, board, score):
+    for r in range(3):
+        for c in range(3):
+            if board[r][c] == ' ':
+                if c > 2:
+                  c = 0
+                  r = r + 1
+                return r, c  # Return the first available move
+
+
+
