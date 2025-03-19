b0daff59f5a0751eeaaf6b31832c24a9832b835f
Kyle Coon <367346@guhsd.net>
2025-03-19 11:57:20 -0700

    another update

--- coon.py
+++ coon.py
@@ -1,6 +1,6 @@
 team_name = 'coon'
-strategy_name = 'CMCS'
-strategy_description = 'It playes the first corner, then the middle, followed by the other corners, and then the sides.'
+strategy_name = 'MCS'
+strategy_description = 'It playes the middle, then the corners, and then the sides'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -10,11 +10,12 @@ def print_board(board):
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
   print(print_board)
 
+
 def move(player, board, score):
-  r = 0
-  c = 0
-  rlist = [2, 1, 0, 1, 2, 2, 0, 1]
-  clist = [1, 2, 1, 0, 2, 0, 2, 1]
+  r = 1
+  c = 1
+  rlist = [2, 1, 0, 1, 2, 2, 0, 0]
+  clist = [1, 2, 1, 0, 2, 0, 2, 0]
 
   while board[r][c] != ' ':
     r = rlist.pop()
