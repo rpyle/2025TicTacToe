3017d6510fe13c390bb05c4442496a22a8080a0b
Shayla Sarmiento <365291@guhsd.net>
2025-03-20 12:02:10 -0700

    added where to go

--- sarmiento.py
+++ sarmiento.py
@@ -1,6 +1,8 @@
 team_name = 'sarmiento'
-strategy_name = 'Last Open'
-strategy_description = 'Play the last open spot.'
+strategy_name = 'Double Down'
+strategy_description = 'Make last call have a win either way.'
+
+import random
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -11,12 +13,21 @@ def print_board(board):
   print(print_board)
 
 def move(player, board, score):
-  r = 2
-  c = 2
-  while board[r][c] != ' ':
-    c = c - 1
-    if c < 0:
-      c = 2
-      r = r - 1
-  
+  r = 0
+  c = 0
+  if board[r][c] != ' ':
+    c = c + 2
+    r = r
+    if board[r][c] != ' ':
+      r = 1
+      c = 1
+      if board[r][c] != ' ':
+        r = 0
+        c = 0
+      if board[r][c] != ' ':
+        r = 2
+        c = c
+        while board[r][c] != ' ':
+          r = random.randint(0,2)
+          c = random.randint(0,2)
   return r, c
