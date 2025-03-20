85ed497bc6cfba6de8a1401d7e0e7467dbeb3b99
ttroy5 <372434@guhsd.net>
2025-03-20 12:06:53 -0700

    Enterd parameters such as player and strategy

--- Witucki.py
+++ Witucki.py
@@ -1,7 +1,7 @@
-
+import random
 team_name = 'Witucki'
 strategy_name = 'corners then next opean'
-strategy_description = 'It will play the corners then the next opean from the strat'
+strategy_description = 'It will play the corners then the next opean from the begining of the board, or it will play the first boxes then the corners'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -12,18 +12,33 @@ def print_board(board):
   print(print_board)
 
 def move(player, board, score):
-   
-   corners = [(0,0), (0,2), (2,0), (2,2)]
-   corners = [(r,c) for r, c in corners if board [r][c] == ""]
-   if corners:
-      return corners[0]
-   r =0
-   c =0
-   while board[r][c] != ' ':
-    c = c + 1
-    if c > 2:
-      c = 0
-      r = r + 1
+   player = random.randint(0,1)
+   while player == 1:
+    corners = [(0,0), (0,2), (2,0), (2,2)]
+    corners = [(r,c) for r, c in corners if board [r][c] == ""]
+    if corners:
+        return corners[0]
+    r =0
+    c =0
+    while board[r][c] != ' ':
+      c = c + 1
+      if c > 2:
+        c = 0
+        r = r + 1
+   while player == 0:
+     r =0
+     c =0
+     while board[r][c] != ' ':
+       c = c + 1
+       if c > 2:
+         c = 0
+         r = r + 1
+
+     corners = [(0,0), (0,2), (2,0), (2,2)]
+     corners = [(r,c) for r, c in corners if board [r][c] == ""]
+     if corners:
+        return corners[0]
+     
 
   
    return r, c
