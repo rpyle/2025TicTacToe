d4ab24181ce7aa0b00aa652a1fc7ba236d373ac5
Violet Keith <366075@guhsd.net>
2025-03-21 11:22:56 -0700

    made worse

--- concat_tester.py
+++ concat_tester.py
@@ -1,50 +1,64 @@
-import random as r
-test_board = [['X', 'O', ' '],
-              [' ', ' ', 'O'],
-              ['X', ' ', 'X']]
-#north = test_board[0][0]+test_board[1][0]+test_board[2][0]
-#east = test_board[2][0]+test_board[2][1]+test_board[2][1]
-#south = test_board[2][0]+test_board[2][1]+test_board[2][1]
-#west = test_board[0][0]+test_board[0][1]+test_board[0][1]
-#vertical = test_board[0][1]+test_board[1][1]+test_board[2][1]
-#horizontal = test_board[1][0]+test_board[1][1]+test_board[1][2]
-#dexter = test_board[0][0]+test_board[1][1]+test_board[2][2]
-#sinister = test_board[2][0]+test_board[1][1]+test_board[0][2]
+import random as rd
+test_board = [['X', 'O', 'X'],
+              ['X', ' ', 'O'],
+              ['O', 'O', 'X']]
 def placeholder(player, board, score):
+    r = 0
+    c = 0
     ready = False
     if player == "X":
         enemy = "O"
     else:
         enemy = "X"
     check = 0 # to make sure it doesn't check forever
-    strat_list = [[board[0][0],board[1][0],board[2][0]], # 0 north
-                  [board[2][0],board[2][1],board[2][1]], # 1 east
-                  [board[2][0],board[2][1],board[2][1]], # 2 south
-                  [board[0][0],board[0][1],board[0][1]], # 3 west
+    strat_list = [[board[0][0],board[0][1],board[0][2]], # 0 north
+                  [board[0][2],board[1][2],board[2][2]], # 1 east
+                  [board[2][0],board[2][1],board[2][2]], # 2 south
+                  [board[0][0],board[1][0],board[2][0]], # 3 west
                   [board[0][1],board[1][1],board[2][1]], # 4 vertical
                   [board[1][0],board[1][1],board[1][2]], # 5 horizontal
                   [board[0][0],board[1][1],board[2][2]], # 6 dexter
-                  [board[2][0],board[1][1],board[0][2]]] # 7 sinister
-    rowfinder = 0
-    while ready == False and check != 7:
-        s = r.randint(0,7) # pick one at random--if you detect a non-empty, non-player token in that line, switch over
+                  [board[0][2],board[1][1],board[2][0]]] # 7 sinister
+    rowfinder = [[0,0,0],
+                 [0,1,2],
+                 [2,2,2],
+                 [0,1,2],
+                 [0,1,2],
+                 [1,1,1],
+                 [0,1,2],
+                 [0,1,2]]
+    # print(strat_list)
+    while ready == False and check != 10:
+        s = rd.randint(0,7) # pick one at random--if you detect a non-empty, non-player token in that line, switch over
         line = strat_list[s][0]+strat_list[s][1]+strat_list[s][2]
-        print(s)
-        print(line)
+        # print(s)
+        # print(line)
         detect = line.find(enemy)
         aim = line.find(" ")
-        print(detect)
-        print(aim)
+        # print(detect)
+        # print(aim)
         if detect == -1:
-            print("No opponent pieces found.")
+            # print("No opponent pieces found.")
             if aim != -1:
-                print("Location found!")
+                # print("Location found!")
                 ready = True
+                check == 0
+                r = rowfinder[s][aim]
+                c = aim
+                # print("Setting token at", r, c)
+                return r, c
             else:
-                print("No empty spots. Switching strategies...")
+                # print("No empty spots. Switching strategies...")
+                check += 1
         else:
-            print("Opponent found. Switching strategies...")
+            # print("Opponent found. Switching strategies...")
             check += 1
-    if check == 7:
-        print("No available wins. Switching to random...")
+    if check == 10:
+        # print("No available wins. Switching to Next Open...")
+        while board[r][c] != ' ':
+          c = c + 1
+          if c > 2:
+            c = 0
+            r = r + 1
+
 placeholder('X',test_board,0)
--- keith.py
+++ keith.py
@@ -1,7 +1,7 @@
 import random
 team_name = 'keith'
-strategy_name = 'test'
-strategy_description = 'Check outside for wins; otherwise random.'
+strategy_name = 'Reference'
+strategy_description = 'Check all combos for wincon; otherwise, Next Open.'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -12,6 +12,61 @@ def print_board(board):
   print(print_board)
  
 def move(player, board, score):
-  r = 1
-  c = 1
-  return r, c
+    r = 0
+    c = 0
+    ready = False
+    if player == "X":
+        enemy = "O"
+    else:
+        enemy = "X"
+    check = 0 # to make sure it doesn't check forever
+    strat_list = [[board[0][0],board[0][1],board[0][2]], # 0 north
+                  [board[0][2],board[1][2],board[2][2]], # 1 east
+                  [board[2][0],board[2][1],board[2][2]], # 2 south
+                  [board[0][0],board[1][0],board[2][0]], # 3 west
+                  [board[0][1],board[1][1],board[2][1]], # 4 vertical
+                  [board[1][0],board[1][1],board[1][2]], # 5 horizontal
+                  [board[0][0],board[1][1],board[2][2]], # 6 dexter
+                  [board[0][2],board[1][1],board[2][0]]] # 7 sinister
+    rowfinder = [[0,0,0],
+                 [0,1,2],
+                 [2,2,2],
+                 [0,1,2],
+                 [0,1,2],
+                 [1,1,1],
+                 [0,1,2],
+                 [0,1,2]]
+    # print(strat_list)
+    while ready == False and check != 10:
+        s = random.randint(0,7) # pick one at random--if you detect a non-empty, non-player token in that line, switch over
+        line = strat_list[s][0]+strat_list[s][1]+strat_list[s][2]
+        # print(s)
+        # print(line)
+        detect = line.find(enemy)
+        aim = line.find(" ")
+        # print(detect)
+        # print(aim)
+        if detect == -1:
+            # print("No opponent pieces found.")
+            if aim != -1:
+                # print("Location found!")
+                ready = True
+                check == 0
+                r = rowfinder[s][aim]
+                c = aim
+                # print("Setting token at", r, c)
+                return r, c
+            else:
+                # print("No empty spots. Switching strategies...")
+                check += 1
+        else:
+            # print("Opponent found. Switching strategies...")
+            check += 1
+    if check == 10:
+        # print("No available wins. Switching to Next Open...")
+        while board[r][c] != ' ':
+          c = c + 1
+          if c > 2:
+            c = 0
+            r = r + 1
+        return r, c
