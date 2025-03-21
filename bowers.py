c697f5a498c6d9449244162a3741a4eb21f1b9da
Brandon Bowers <371925@guhsd.net>
2025-03-21 11:57:47 -0700

    updated bowers stratgy

--- bowers.py
+++ bowers.py
@@ -1,7 +1,7 @@
 team_name = 'bowers'
-strategy_name = 'center then colums, rows'
-strategy_description = 'Play the next open spot.'
-
+strategy_name = 'center, , block and corner, lines '
+strategy_description = 'Play the next o.'
+import random 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
   print('-+-+-')
@@ -11,12 +11,61 @@ def print_board(board):
   print(print_board)
 
 def move(player, board, score):
-  r = 0
-  c = 0
-  while board[r][c] != ' ':
-    c = c + 1
-    if c > 2:
-      c = 0
-      r = r + 1
-  
+  if player == 'X':
+     other_player = 'O'
+  else:
+     other_player = "X"
+     r = 1
+     c = 1
+  #if center is open
+  if board[1][1] == ' ':
+    r = 2
+    c = 2
+    if  board [1][1] ==  (player):
+      if board [0][2] == ' ' or board [0][2] == player :
+       r = 0
+       c = 2
+       if board [2][0] == ' ':
+         r = 2 
+         c = 0 
+      if board [0][0] == ' ':
+        r = 0
+        c = 0 
+        if board[2][2] == ' ':
+          r = 2
+          c = 2
+  #if senter is not open
+  else: 
+    if board[0][0] == ' ':
+      r = 0
+      c = 0 
+      if board[0][1] == ' ' or board[0][1] == (player)   and  board[0][2] == ' ' or board[0][2] == (player) :
+        r=0
+        c=1
+        if board[0][2] == ' ':
+          r=0
+          c=2
+      if board[1][0] == ' '  and board[2][0] == ' ':
+        r=1
+        c=0
+        if board[2][0] == ' ':
+          r=2
+          c=2
+    if board[2][2] == ' ':
+      r = 2
+      c = 2 
+      if board[2][1] == ' ' and board[2][0] == ' ':
+        r=2
+        c=1
+        if board[2][0] == ' ':
+          r=2
+          c=0
+      if board[1][2] == ' ' and board[0][2] == ' ':
+        r=1
+        c=2
+        if board[0][2] == ' ':
+          r=0
+          c=2
+    
+    
   return r, c
