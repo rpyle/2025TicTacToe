3451a080aa0c7ddad7ca23fd12bec7d7bdd0355a
ben <benalcantara2007@gmail.com>
2025-03-20 18:22:54 -0700

    Finalized offensive strategy

--- alcantara.py
+++ alcantara.py
@@ -1,6 +1,6 @@
 team_name = 'alcantara'
 strategy_name = 'The Center Corner Capture'
-strategy_description = 'Take center, corners, complete series'
+strategy_description = 'Take center, corners, midpoints'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -20,6 +20,7 @@ def move(player, board, score):
   r = 0
   c = 0
 
+  #takes center if unoccupied
   if board[1][1] == ' ':
     r = 1
     c = 1
@@ -27,14 +28,34 @@ def move(player, board, score):
   
   #offense
   if player == "X":
-  
-    while corners != 0:
-      choice = random(board)
+    while(r == 1) and (c == 1):
+      taken_corner = 0
       
-      
-      
+      if taken_corner == 4:
+        while board[row][column] != " ":
+          choice = random(midpoints)
+          row = choice[0]
+          column = choice[1]
+        
+        r = row
+        c = column
+        return r, c
+        
+        #take midpoints at random
+        
+      elif board[row][column] == ' ':
+        choice = random(corners)
+        row = choice[0]
+        column = choice[1]
+        
+        r = row
+        c = column
+        return r, c
       else:
-        # TODO
+        taken_corner += 1
+
+      
+
 
   #defense - selects a spot at random; taken from file example2.py
   else:
@@ -43,3 +64,4 @@ def move(player, board, score):
       c = random.randint(0,2)
   
   return r, c
+
